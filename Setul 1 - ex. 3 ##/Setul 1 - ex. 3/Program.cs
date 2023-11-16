74395b1a6a61fb6b9d0b326600ce2b010985db24
VieriuAniko <146959409+VieriuAniko@users.noreply.github.com>
2023-11-16 15:09:40 +0200

    Add files via upload

---
+++ Setul 1 - ex. 3 ##/Setul 1 - ex. 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Setul_1___ex._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
+
+            int n, k;
+
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            k = int.Parse(Console.ReadLine());
+
+            if (n % k == 0)
+                Console.WriteLine($"{n} se divide cu {k}.");
+            else
+                Console.WriteLine($"{n} nu se divide cu {k}.");
+        }
+    }
+}
---
+++ Setul 1 - ex. 6 ##/Setul 1 - ex. 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Setul_1___ex._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: Determinati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. 
+
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (a < b + c && b < a + c && c < a + b)
+                    Console.WriteLine($"Numerele {a}, {b} si {c} pot fi laturile unui triunghi.");
+                else
+                    Console.WriteLine($"Numerele {a}, {b} si {c} nu pot fi laturile unui triunghi.");
+            }
+            else
+                Console.WriteLine($"Numerele {a}, {b} si {c} nu pot fi laturile unui triunghi.");
+        }
+    }
+}
