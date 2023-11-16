678fe077cb573c842af762e9f3e78e5c26d2f27a
VieriuAniko <146959409+VieriuAniko@users.noreply.github.com>
2023-11-16 15:36:49 +0200

    Add files via upload

---
+++ Setul 1 - ex. 21 ##/Setul 1 - ex. 21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Setul_1___ex._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: Ghiciti un numar intre 1 si 1024 prin intrebari de forma "numarul este mai mare sau egal decat x?". 
+
+            Random random = new Random();
+            int numarul = random.Next(1, 1025);
+            bool gasit = false;
+
+            while (!gasit)
+            {
+                Console.WriteLine("Introdu numarul care crezi ca e potrivit.");
+
+                if (int.TryParse(Console.ReadLine(), out int ghiciUser))
+                {
+                    if (ghiciUser < numarul)
+                        Console.WriteLine("Numarul este mai mare. Mai incearca.");
+                    else
+                        if (ghiciUser > numarul)
+                        Console.WriteLine("Numarul este mai mic. Mai incearca.");
+                    else
+                    {
+                        gasit = true;
+                        Console.WriteLine("Felicitari! Ai ghicit numarul :)");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Numarul introdus nu este valid. Incearca din nou.");
+                }
+            }
+        }
+    }
+}
